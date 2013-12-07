e8528a76ec39b21eb515dac0cea7e803d039809a
Jeremy Duvall <matchless.tone@gmail.com>
2013-12-07 18:06:03 -0500

    More tests

---
+++ TestProjects/ClassLibrary1/InterfaceTestAssembly1/Class2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTestAssembly1
+{
+    public class Class2
+    {
+        public void Hello()
+        {
+            var toPrint = "Hello world hello";
+            Console.WriteLine(toPrint);
+        }
+
+        public int Boo()
+        {
+            var lowerBound = 10;
+            var rand = new Random();
+            return rand.Next(lowerBound, 100) * 100;
+        }
+    }
+}
