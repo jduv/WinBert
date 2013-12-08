1657a74b2716acd9514feb7db1db5d6d12b26095
Jeremy Duvall <matchless.tone@gmail.com>
2013-12-08 18:04:06 -0500

    Analysis engine unit tests

---
+++ Arktos.WinBert.UnitTests/AnalysisResultUnitTests.cs
@@ -0,0 +1,222 @@
+﻿namespace Arktos.WinBert.UnitTests
+{
+    using AppDomainToolkit;
+    using Arktos.WinBert.Analysis;
+    using Arktos.WinBert.Testing;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using System;
+
+    [TestClass]
+    public class AnalysisResultUnitTests
+    {
+        #region Fields & Constants
+
+        private Mock<IAssemblyTarget> targetOneMock;
+        private Mock<IAssemblyTarget> targetTwoMock;
+        private Mock<ITestRunResult> simpleSuccessfulTestResultMock;
+        private Mock<ITestRunResult> simpleUnsuccessfulTestResultMock;
+
+        #endregion
+
+        #region Test Plumbing
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            this.targetOneMock = new Mock<IAssemblyTarget>();
+            this.targetOneMock.Setup(x => x.Location).Returns(@"C:\MyLocation\MyAssembly1.dll");
+            this.targetOneMock.Setup(x => x.FullName).Returns("MyAssembly1.dll");
+
+            this.targetTwoMock = new Mock<IAssemblyTarget>();
+            this.targetTwoMock.Setup(x => x.Location).Returns(@"C:\MyLocation\MyAssembly2.dll");
+            this.targetTwoMock.Setup(x => x.FullName).Returns("MyAssembly2.dll");
+
+            this.simpleSuccessfulTestResultMock = new Mock<ITestRunResult>();
+            simpleSuccessfulTestResultMock.Setup(x => x.Success).Returns(true);
+            simpleSuccessfulTestResultMock.Setup(x => x.PathToAnalysisLog).Returns(string.Empty);
+            simpleSuccessfulTestResultMock.Setup(x => x.Target).Returns(this.targetOneMock.Object);
+
+            this.simpleUnsuccessfulTestResultMock = new Mock<ITestRunResult>();
+            simpleUnsuccessfulTestResultMock.Setup(x => x.Success).Returns(false);
+            simpleUnsuccessfulTestResultMock.Setup(x => x.PathToAnalysisLog).Returns(string.Empty);
+            simpleUnsuccessfulTestResultMock.Setup(x => x.Target).Returns(this.targetOneMock.Object);
+        }
+
+        #endregion
+
+        #region Test Methods
+
+        #region UnsuccessfulTestRun
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void UnsuccessfulTestRun_NullArguments()
+        {
+            AnalysisResult.UnsuccessfulTestRun(null, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void UnsuccessfulTestRun_NullPreviousTestRun()
+        {
+            AnalysisResult.UnsuccessfulTestRun(null, this.simpleSuccessfulTestResultMock.Object);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void UnsuccessfulTestRun_NullCurrentTestRun()
+        {
+            AnalysisResult.UnsuccessfulTestRun(this.simpleSuccessfulTestResultMock.Object, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void UnsuccessfulTestRun_SuccessfulRuns()
+        {
+            AnalysisResult.UnsuccessfulTestRun(
+                this.simpleSuccessfulTestResultMock.Object,
+                this.simpleSuccessfulTestResultMock.Object);
+        }
+
+        [TestMethod]
+        public void UnsuccessfulTestRun_UnsuccessfulPreviousTestRun()
+        {
+            var unsuccessfulMock = new Mock<ITestRunResult>();
+            unsuccessfulMock.Setup(x => x.Success).Returns(false);
+            unsuccessfulMock.Setup(x => x.PathToAnalysisLog).Returns(string.Empty);
+            unsuccessfulMock.Setup(x => x.Target).Returns(this.targetOneMock.Object);
+
+            var successfulMock = new Mock<ITestRunResult>();
+            successfulMock.Setup(x => x.Success).Returns(true);
+            successfulMock.Setup(x => x.PathToAnalysisLog).Returns(string.Empty);
+            successfulMock.Setup(x => x.Target).Returns(this.targetTwoMock.Object);
+
+            var result = AnalysisResult.UnsuccessfulTestRun(unsuccessfulMock.Object, successfulMock.Object);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(InconclusiveAnalysisResult));
+
+            var typedResult = result as InconclusiveAnalysisResult;
+            Assert.IsNotNull(typedResult.Message);
+            Assert.IsTrue(typedResult.Message.Contains(AnalysisResult.SingleRunUnsuccessfulMessage));
+            Assert.IsTrue(typedResult.Message.Contains(this.targetOneMock.Object.Location));
+            Assert.IsFalse(typedResult.Message.Contains(this.targetTwoMock.Object.Location));
+        }
+
+        [TestMethod]
+        public void UnsuccessfulTestRun_UnsuccessfulCurrentTestRun()
+        {
+            var unsuccessfulMock = new Mock<ITestRunResult>();
+            unsuccessfulMock.Setup(x => x.Success).Returns(false);
+            unsuccessfulMock.Setup(x => x.PathToAnalysisLog).Returns(string.Empty);
+            unsuccessfulMock.Setup(x => x.Target).Returns(this.targetOneMock.Object);
+
+            var successfulMock = new Mock<ITestRunResult>();
+            successfulMock.Setup(x => x.Success).Returns(true);
+            successfulMock.Setup(x => x.PathToAnalysisLog).Returns(string.Empty);
+            successfulMock.Setup(x => x.Target).Returns(this.targetTwoMock.Object);
+
+            var result = AnalysisResult.UnsuccessfulTestRun(successfulMock.Object, unsuccessfulMock.Object);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(InconclusiveAnalysisResult));
+
+            var typedResult = result as InconclusiveAnalysisResult;
+            Assert.IsNotNull(typedResult.Message);
+            Assert.IsTrue(typedResult.Message.Contains(AnalysisResult.SingleRunUnsuccessfulMessage));
+            Assert.IsTrue(typedResult.Message.Contains(this.targetOneMock.Object.Location));
+            Assert.IsFalse(typedResult.Message.Contains(this.targetTwoMock.Object.Location));
+        }
+
+        [TestMethod]
+        public void UnsuccessfulTestRun_BothRunsUnsuccessful()
+        {
+            var unsuccessfulMock1 = new Mock<ITestRunResult>();
+            unsuccessfulMock1.Setup(x => x.Success).Returns(false);
+            unsuccessfulMock1.Setup(x => x.PathToAnalysisLog).Returns(string.Empty);
+            unsuccessfulMock1.Setup(x => x.Target).Returns(this.targetOneMock.Object);
+
+            var unsuccessfulMock2 = new Mock<ITestRunResult>();
+            unsuccessfulMock2.Setup(x => x.Success).Returns(false);
+            unsuccessfulMock2.Setup(x => x.PathToAnalysisLog).Returns(string.Empty);
+            unsuccessfulMock2.Setup(x => x.Target).Returns(this.targetTwoMock.Object);
+
+            var result = AnalysisResult.UnsuccessfulTestRun(unsuccessfulMock1.Object, unsuccessfulMock2.Object);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(InconclusiveAnalysisResult));
+
+            var typedResult = result as InconclusiveAnalysisResult;
+            Assert.IsNotNull(typedResult.Message);
+            Assert.IsTrue(typedResult.Message.Contains(AnalysisResult.BothRunsUnsuccessfulMessage));
+            Assert.IsTrue(typedResult.Message.Contains(this.targetOneMock.Object.Location));
+            Assert.IsTrue(typedResult.Message.Contains(this.targetTwoMock.Object.Location));
+        }
+
+        #endregion
+
+        #region NoDifference
+
+        [TestMethod]
+        public void NoDifference()
+        {
+            var result = AnalysisResult.NoDifference();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(InconclusiveAnalysisResult));
+
+            var typedResult = result as InconclusiveAnalysisResult;
+            Assert.IsNotNull(typedResult.Message);
+            Assert.AreEqual(typedResult.Message, AnalysisResult.NoDifferenceMessage, true);
+        }
+
+        #endregion
+
+        #region UnknownError
+
+        [TestMethod]
+        public void UnknownError()
+        {
+            var result = AnalysisResult.UnknownError();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(InconclusiveAnalysisResult));
+
+            var typedResult = result as InconclusiveAnalysisResult;
+            Assert.IsNotNull(typedResult.Message);
+            Assert.AreEqual(typedResult.Message, AnalysisResult.UnknownErrorMessage, true);
+        }
+
+        #endregion
+
+        #region FromException
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FromException_NullArgument()
+        {
+            AnalysisResult.FromException(null);
+        }
+
+        [TestMethod]
+        public void FromException()
+        {
+            var message = "My test message";
+            var exception = new ArgumentException(message);
+            var result = AnalysisResult.FromException(exception);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(InconclusiveAnalysisResult));
+
+            var typedResult = result as InconclusiveAnalysisResult;
+            Assert.IsNotNull(typedResult.Message);
+            Assert.IsTrue(typedResult.Message.Contains(AnalysisResult.ExceptionMessage));
+            Assert.IsTrue(typedResult.Message.Contains(message));
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
--- Arktos.WinBert.UnitTests/BertBehavioralAnalyzerUnitTests.cs
+++ Arktos.WinBert.UnitTests/BertBehavioralAnalyzerUnitTests.cs
@@ -8,6 +8,7 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Moq;
     using System;
+    using System.Reflection;
 
     [TestClass]
     [DeploymentItem("test-analysis", "test-analysis")]
@@ -16,11 +17,10 @@
         #region Fields & Constants
 
         private static readonly string AnalysisDir = @"test-analysis\";
-        private static readonly string newAnalysisPath = AnalysisDir + "analysisNew.xml";
-        private static readonly string oldAnalysisPath = AnalysisDir + "analysisOld.xml";
+        private static readonly string NewAnalysisPath = AnalysisDir + "analysisNew.xml";
+        private static readonly string OldAnalysisPath = AnalysisDir + "analysisOld.xml";
 
         private Mock<IFileSystem> fileSystemMock;
-        private Mock<IAssemblyTarget> targetMock;
         private Mock<ITestRunResult> simpleSuccessfulTestResultMock;
         private Mock<ITestRunResult> simpleUnsuccessfulTestResultMock;
         private Mock<IAssemblyDifferenceResult> differenceMock;
@@ -34,7 +34,6 @@
         public void TestInit()
         {
             this.fileSystemMock = new Mock<IFileSystem>();
-            this.targetMock = new Mock<IAssemblyTarget>();
 
             this.differenceMock = new Mock<IAssemblyDifferenceResult>();
             this.differenceMock.Setup(x => x.IsDifferent).Returns(true);
@@ -42,15 +41,20 @@
             this.noDifferenceMock = new Mock<IAssemblyDifferenceResult>();
             this.noDifferenceMock.Setup(x => x.IsDifferent).Returns(false);
 
+            var simpleTargetMock = new Mock<IAssemblyTarget>();
+            simpleTargetMock.Setup(x => x.Location).Returns(Assembly.GetExecutingAssembly().Location);
+            simpleTargetMock.Setup(x => x.FullName).Returns(Assembly.GetExecutingAssembly().FullName);
+            simpleTargetMock.Setup(x => x.CodeBase).Returns(new Uri(Assembly.GetExecutingAssembly().CodeBase));
+
             this.simpleSuccessfulTestResultMock = new Mock<ITestRunResult>();
             simpleSuccessfulTestResultMock.Setup(x => x.Success).Returns(true);
             simpleSuccessfulTestResultMock.Setup(x => x.PathToAnalysisLog).Returns(string.Empty);
-            simpleSuccessfulTestResultMock.Setup(x => x.Target).Returns(this.targetMock.Object);
+            simpleSuccessfulTestResultMock.Setup(x => x.Target).Returns(simpleTargetMock.Object);
 
             this.simpleUnsuccessfulTestResultMock = new Mock<ITestRunResult>();
             simpleUnsuccessfulTestResultMock.Setup(x => x.Success).Returns(false);
             simpleUnsuccessfulTestResultMock.Setup(x => x.PathToAnalysisLog).Returns(string.Empty);
-            simpleUnsuccessfulTestResultMock.Setup(x => x.Target).Returns(this.targetMock.Object);
+            simpleUnsuccessfulTestResultMock.Setup(x => x.Target).Returns(simpleTargetMock.Object);
         }
 
         #endregion
@@ -106,8 +110,60 @@
         public void Analyze_UnuccessfulTestRunResult()
         {
             var target = new BertBehavioralAnalyzer(this.fileSystemMock.Object);
-            var result = target.Analyze(this.differenceMock.Object, this.simpleUnsuccessfulTestResultMock.Object, this.simpleSuccessfulTestResultMock.Object);
-            // TODO: Write me.
+            var result = target.Analyze(
+                this.differenceMock.Object,
+                this.simpleUnsuccessfulTestResultMock.Object,
+                this.simpleSuccessfulTestResultMock.Object);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(InconclusiveAnalysisResult));
+        }
+
+        [TestMethod]
+        public void Analyze_BothUnsuccessfulTestRunResults()
+        {
+            var target = new BertBehavioralAnalyzer(this.fileSystemMock.Object);
+            var result = target.Analyze(
+                this.differenceMock.Object,
+                this.simpleUnsuccessfulTestResultMock.Object,
+                this.simpleUnsuccessfulTestResultMock.Object);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(InconclusiveAnalysisResult));
+        }
+
+        [TestMethod]
+        public void Analyze_NoDifferences()
+        {
+            var noDiffMock = new Mock<IAssemblyDifferenceResult>();
+            noDiffMock.Setup(x => x.IsDifferent).Returns(false);
+
+            var target = new BertBehavioralAnalyzer(this.fileSystemMock.Object);
+            var result = target.Analyze(
+                noDiffMock.Object,
+                this.simpleUnsuccessfulTestResultMock.Object,
+                this.simpleUnsuccessfulTestResultMock.Object);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(InconclusiveAnalysisResult));
+        }
+
+        [TestMethod]
+        public void Analyze_SuccessfulRuns()
+        {
+            var newAssemblyResult = new Mock<ITestRunResult>();
+            newAssemblyResult.Setup(x => x.Success).Returns(true);
+            newAssemblyResult.Setup(x => x.PathToAnalysisLog).Returns(NewAnalysisPath);
+
+            var oldAssemblyResult = new Mock<ITestRunResult>();
+            oldAssemblyResult.Setup(x => x.Success).Returns(true);
+            oldAssemblyResult.Setup(x => x.PathToAnalysisLog).Returns(OldAnalysisPath);
+
+            var target = new BertBehavioralAnalyzer(this.fileSystemMock.Object);
+            var result = target.Analyze(this.differenceMock.Object, oldAssemblyResult.Object, newAssemblyResult.Object);
+
+            Assert.IsNotNull(result);
+            Assert.Inconclusive();
         }
 
         #endregion
